db4d3736a7d8658658533842b070d9bf5ff78ac0
Hotaru <37961000+heyhotaru@users.noreply.github.com>
2022-09-01 20:46:49 -0300

    TS431
    LP2 add
    java aula pratica 1

---
+++ TS431/LP2/a1.java
@@ -0,0 +1,7 @@
+public class a1 {
+    public static void main (String [] arg){
+        int a = 3;
+        int b = 2;
+        System.out.println(a+b);
+    }
+}
---
+++ TS431/LP2/smp.java
@@ -0,0 +1,11 @@
+public class smp {
+    public static void main (String [] arg){
+    int a = 2;
+    int b = 3;
+    int c = 4;
+    int soma = a + b + c;
+    float media = (float) soma / 3;
+    int produto = a* b* c;
+    System.out.println("\n\tsoma " + soma +"\n\tmedia " + media + "\n\tproduto " + produto);
+}
+}
