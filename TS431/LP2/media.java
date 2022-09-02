016552426b309400390f3117c2edab9b5c3cef97
Hotaru <37961000+heyhotaru@users.noreply.github.com>
2022-09-01 21:42:36 -0300

    Update Media

--- TS431/LP2/media.java
+++ TS431/LP2/media.java
@@ -5,9 +5,14 @@ public class media {
         float a = Float.parseFloat(JOptionPane.showInputDialog("Digite a primeira nota "));
         float b = Float.parseFloat(JOptionPane.showInputDialog("Digite a segunda nota "));
         float c = Float.parseFloat(JOptionPane.showInputDialog("Digite a terceira nota "));
+        int falta = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade da faltas"));
         float soma = a + b + c;
         float media = soma / 3;
 
-        System.out.println("O nome do aluno é: " + nomeAluno + "\n" + "A média do aluno é: " + media);
+        if(media >= 6 && falta <= 18){
+        System.out.println("O nome do aluno é: " + nomeAluno + "\n" + "A média do aluno é: " + media + "\nAprovado" + "Faltas " + falta);
+        }else{
+            System.out.println("O nome do aluno é: " + nomeAluno + "\n" + "A média do aluno é " + media + "\nREPROVADO" + "Faltas " + falta);
+        }
     }
 }
