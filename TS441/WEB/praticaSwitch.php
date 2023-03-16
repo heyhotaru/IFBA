62f33cf17a21348e2a15c48f84e6953b4454bc6c
Hotaru <37961000+heyhotaru@users.noreply.github.com>
2023-03-16 20:30:33 -0300

    DDD

--- TS441/WEB/praticaSwitch.php
+++ TS441/WEB/praticaSwitch.php
@@ -1,6 +1,37 @@
 <?php
 
+$DDD = readline("Digite o DDD");
 
-echo("Hello WOrld");
+Switch(DDD){
+
+    case 61:
+        echo("O DDD $DDD é de Brasília");
+        break;
+    case 71:
+        echo("O DDD $DDD é de Salvador");
+        break;
+    case 11:
+        echo("O DDD $DDD é de São Paulo");
+        break;
+    case 21:
+        echo("O DDD $DDD é de Rio de Janeiro");
+        break;
+    case 32:
+        echo("O DDD $DDD é de Juiz de Fora");
+        break;
+    case 19:
+        echo("O DDD $DDD é de Campinas");
+        break;
+    case 27:
+        echo("O DDD $DDD é de Vitoria");
+        break;
+    case 31:
+        echo("O DDD $DDD é de Belo Horizonte");
+        break;
+
+    default:
+    echo("DDD $DDD NÃO CADASTRADO");
+}
+
 
 ?>
